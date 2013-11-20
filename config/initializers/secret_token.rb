ac5f79c4cb510c4dbfefe3a3108de97994e2593c
Josh Ogle <joshua@go-ogle.org>
2013-11-19 19:50:45 -0600

    App built and running on bluehost

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tands::Application.config.secret_token = '99634aa6299fa0e19bf9a8b2f140b52c7d697f55fb17a957d825b76e1471b99c43202a11532549af04813acebe413bc664583034bbc8e1c92cae2202cacbf210'
